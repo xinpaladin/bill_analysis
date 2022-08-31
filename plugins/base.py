690b2d8b6a2d647817c06d75c2e2db9f8cd8fe68
zhangx016 <zhangx016@foresealife.com>
2022-08-31 18:02:47 +0800

    commit

---
+++ plugins/base.py
@@ -0,0 +1,11 @@
+from abc import abstractmethod
+
+
+class BaseBillAnalysis(object):
+
+    def __init__(self) -> None:
+        pass
+    
+    @abstractmethod
+    def analysis(self, content):
+        raise NotImplementedError("need to be implemented")
