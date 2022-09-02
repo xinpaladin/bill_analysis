f5191bb0b161a74e37ea45b3defc63d980c6a207
zhangxin <7850151@163.com>
2022-09-02 17:38:10 +0800

    add pre-dommit

--- plugins/base.py
+++ plugins/base.py
@@ -2,10 +2,9 @@ from abc import abstractmethod
 
 
 class BaseBillAnalysis(object):
-
     def __init__(self) -> None:
         pass
-    
+
     @abstractmethod
     def analysis(self, content):
         raise NotImplementedError("need to be implemented")
